e760408324e7fbc956ee37aa0770efa1f7636001
Anson Ng <cwnga@yahoo-inc.com>
2016-06-20 12:39:44 +0800

    add ADKNibSizeCalculator example

---
+++ Tutorials/ADKNibSizeCalculator/ADKNibSizeCalculator/DemoCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  DemoCollectionViewCell.h
+//  ADKNibSizeCalculator
+//
+//  Created by Anson Ng on 6/20/16.
+//  Copyright © 2016 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+@end
