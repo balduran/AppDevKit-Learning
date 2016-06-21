4c138e8e732775361c24eae420e3b3abbabc40e3
anistar sung <cfsung@yahoo-inc.com>
2016-06-21 19:00:23 +0800

    Add a sample cell for ADKNibSizeCalculator

---
+++ Tutorials/ADKNibSizeCalculator/ADKNibSizeCalculator/SampleCollectionVIewCellCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SampleCollectionVIewCellCollectionViewCell.h
+//  ADKNibSizeCalculator
+//
+//  Created by Chih Feng Sung on 6/21/16.
+//  Copyright © 2016 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleCollectionVIewCellCollectionViewCell : UICollectionViewCell
+
+@end
